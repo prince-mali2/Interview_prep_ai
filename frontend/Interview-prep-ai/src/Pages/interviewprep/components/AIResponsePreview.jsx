2051f18ea02581a6501dc2b16e653670cdc86102
prince-mali2 <princemali019@gmail.com>
2025-09-26 13:18:40 +0530

    fix: corrected react-markdown import

--- frontend/Interview-prep-ai/src/Pages/interviewprep/components/AIResponsePreview.jsx
+++ frontend/Interview-prep-ai/src/Pages/interviewprep/components/AIResponsePreview.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 import {LuCopy, LuCheck, LuCode} from 'react-icons/lu';
-import ReactMarkdown from 'react-Markdown';
+import ReactMarkdown from "react-markdown";
 import remarkGfm from 'remark-gfm'
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import {oneLight} from 'react-syntax-highlighter/dist/esm/styles/prism'
