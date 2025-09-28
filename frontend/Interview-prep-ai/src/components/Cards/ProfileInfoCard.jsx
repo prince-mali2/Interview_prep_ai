2a3142c7ba8272cb01364eb2432ab243817787e4
prince-mali2 <princemali019@gmail.com>
2025-09-28 11:34:34 +0530

    fix: corrected react-markdown import

--- frontend/Interview-prep-ai/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/Interview-prep-ai/src/components/Cards/ProfileInfoCard.jsx
@@ -1,40 +1,43 @@
-import React,{useContext} from 'react'
-import {useNavigate} from 'react-router-dom';
-import {UserContext} from '../../context/userContext';
-
-
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/userContext';
+import DUMMY_AVATAR from "../../assets/dummy.jpeg"; 
 
 const ProfileInfoCard = () => {
-    const {user, clearUser} = useContext(UserContext);
-    const navigate = useNavigate
-    ();
+  const { user, clearUser } = useContext(UserContext);
+  const navigate = useNavigate();
 
-    const handleLogOut = () =>{
-      localStorage.clear();
-      clearUser();
-      navigate("/");
-    }
-     
+  const handleLogOut = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/");
+  };
 
-  return user && (
-    
-    <div className="flex items-center">
-      {user && 
-      <img src={user.profileImageUrl} alt="Profile Image" className="w-11 h-11 gb-gray-300 rounded-full mr-3" />
-      }
+  return (
+    user && (
+      <div className="flex items-center">
+        {/* Profile Image with fallback */}
+        <img
+          src={!user?.profileImageUrl ? DUMMY_AVATAR : user.profileImageUrl  }
+          alt="Dummy"
+          className="w-11 h-11 bg-gray-300 rounded-full mr-3 object-cover border border-gray-200"
+        />
 
-      <div>
-        <div className="text=[15px] text-black font-bold leading-3" >
-          {user.name}
+        {/* User Info */}
+        <div>
+          <div className="text-[15px] text-black font-bold leading-4">
+            {user?.name}
+          </div>
+          <button
+            className="text-amber-600 text-sm font-semibold cursor-pointer hover:underline"
+            onClick={handleLogOut}
+          >
+            Logout
+          </button>
         </div>
-        <button className="text-amber-600 text-sm font-semibold cursor-pointer hover:underline"
-        onClick={handleLogOut}
-        >
-          Logout
-        </button>
       </div>
-    </div>
-  )
-}
+    )
+  );
+};
 
-export default ProfileInfoCard
+export default ProfileInfoCard;
